e166250897c6a4c1ffd9ca48e85806a6a7b80fbe
Kartik Saini <sainikartik024@gmail.com>
2026-02-14 13:29:00 +1100

    cleaning config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 import emailServerPlugin from "./plugins/vite-email-server";
 import rcmProxyPlugin from "./plugins/vite-rcm-proxy";
 
@@ -15,8 +15,7 @@ export default defineConfig(({ mode }) => ({
     emailServerPlugin(),
     rcmProxyPlugin(),
     react(),
-    mode === 'development' &&
-    componentTagger(),
+
   ].filter(Boolean),
   resolve: {
     alias: {
